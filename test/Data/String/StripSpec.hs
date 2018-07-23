4a83154e23d0810ef36614d428fbc9c3571dcab3
Simon Hengel <sol@typeful.net>
2018-07-23 16:27:14 +0800

    Remove Data.String.StripSpec.main

--- test/Data/String/StripSpec.hs
+++ test/Data/String/StripSpec.hs
@@ -1,12 +1,7 @@
-module Data.String.StripSpec (main, spec) where
+module Data.String.StripSpec (spec) where
 
 import Test.Hspec
 import Data.String.Strip
-
--- `main` is here so that this module can be run from GHCi on its own.  It is
--- not needed for automatic spec discovery.
-main :: IO ()
-main = hspec spec
 
 spec :: Spec
 spec = do
